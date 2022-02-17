c0ca8aae28435ef4a24af35b778357ae982fdfb5
Brendan Merritt <brm2000@live.unc.edu>
2022-02-17 17:02:07 -0500

    finished all requirements for a03

--- server.js
+++ server.js
@@ -15,12 +15,6 @@ const server = app.listen(port, () => {
     //or console.log('App is running on port %PORT%').replace('%PORT')
 });
 
-//default endpoint
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND');
-    res.type('text/plain');
-});
-
 //define check endpoint
 app.get('/app/', (req, res) => {
     //respond with status 200
@@ -29,22 +23,37 @@ app.get('/app/', (req, res) => {
     //respond with status message "OK"
     res.statusMessage = 'OK';
     res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-    res.end(res.statusCode+ ' ' +res.statusMessage);
+    res.end(res.statusCode + ' ' + res.statusMessage);
 });
 
 //random coin flip endpoint
-app.get('app/flip', (req, res) => {
-    res.json({ 'flip': coin.coinFlip() })
-    res.statusCode = 200;
-    res.statusMessage = 'OK';
-    res.writeHead(res.statusCode, { 'Content-Type' : 'text/plain' });
-    res.end(res.statusCode + ' ' + resizeTo.statusMessage);
+app.get('/app/flip', (req, res) => {
+    res.contentType('text/json');
+    res.status(200).json({ 'flip' : coin.coinFlip()});
 });
-
-
 
 //multiple flips endpoint
 app.get('/app/flips/:number', (req, res) => {
-	const flips = manyflips(req.params.number);
+    res.contentType('text/json');
+	const flips = coin.coinFlips(req.params.number);
+    const count = coin.countFlips(flips);
+    res.status(200).json({ 'raw' : flips, 'summary' : count})
+});
 
+//flip match against heads endpoint
+app.get('/app/flip/call/heads', (req, res) => {
+    res.contentType('text/json');
+    res.status(200).json(coin.flipACoin('heads'));
+});
+
+
+//flip match against tails endpoint
+app.get('/app/flip/call/tails', (req, res) => {
+    res.contentType('text/json');
+    res.status(200).json(coin.flipACoin('tails'));
+});
+
+//default endpoint
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND');
 });
